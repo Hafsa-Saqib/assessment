d29798cea42b1e575ba954c6b281ea8c346382d9
Hafsa-Saqib <hafsasaquib1999@gmail.com>
2021-12-02 12:53:42 +0500

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,61 @@
+// @dart=2.9
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+import 'package:assesment/services.dart';
 import 'package:flutter/material.dart';
-import 'package:assessment/screens/home_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'HOTEL',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        scaffoldBackgroundColor: Color(0xFFF3F5F7),
+        primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  APIService apiService = APIService();
+// You future
+  Future future;
+//in the initState() or use it how you want...
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    future = apiService.get(endpoint:"GET",query:{"radius":"100"});
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+
+      ),
     );
   }
 }
